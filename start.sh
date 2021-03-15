4bd7a3cd580600e5a7890a9f7ab5ed3fe2befbcf
laghari4u <49945764+laghari4u@users.noreply.github.com>
2021-03-15 10:21:48 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qgc3uymavtjztjprfhk4knt73ccprl8htwjsldt.test1
+./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1q8cjnpt0g0fee2wdq2qsxrql2dnj2ets4asd27s.test1
 
 
 done
